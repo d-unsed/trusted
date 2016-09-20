b1de188d1645bb5e7f35931ade44a3ca359f06f3
Dmitry Gritsay <unseductable@gmail.com>
2016-09-20 22:22:55 +0300

    Load extension on gem load

--- lib/trusted.rb
+++ lib/trusted.rb
@@ -1,5 +1,15 @@
 require 'trusted/version'
 
+require 'fiddle'
+
+library_path = File.expand_path(
+  File.join(File.dirname(__FILE__), '../ext/trusted/target/release/libtrusted.dylib')
+)
+
+library = Fiddle::dlopen(library_gpath)
+function = Fiddle::Function.new(library['initialize_my_app'], [], Fiddle::TYPE_VOIDP)
+function.call
+
 module Trusted
   # Your code goes here...
 end
