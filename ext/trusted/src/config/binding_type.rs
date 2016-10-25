949254ca222401ce86542105a0b295fc028de71d
Dmitry Gritsay <unseductable@gmail.com>
2016-10-25 22:34:58 +0300

    Implement From<&str> for BindingType

--- ext/trusted/src/config/binding_type.rs
+++ ext/trusted/src/config/binding_type.rs
@@ -5,3 +5,30 @@ pub enum BindingType {
     Tcp,
     Unix,
 }
+
+impl<'a> From<&'a str> for BindingType {
+    fn from(string: &str) -> Self {
+        match string {
+            "tcp" => BindingType::Tcp,
+            "unix" => BindingType::Unix,
+
+            // TODO: move to `Result` and return `Err`
+            _ => panic!("Unsupported binding type"),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_is_converted_from_tcp() {
+        assert_eq!(BindingType::from("tcp"), BindingType::Tcp);
+    }
+
+    #[test]
+    fn it_is_converted_from_unix() {
+        assert_eq!(BindingType::from("unix"), BindingType::Unix);
+    }
+}
