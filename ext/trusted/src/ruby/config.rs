c0bee147caf3daa34cf73f492845e3cc7ef181d7
Dmitry Gritsay <unseductable@gmail.com>
2016-10-25 23:06:21 +0300

    Implement VerifiedObject for ruby::Config

--- ext/trusted/src/ruby/config.rs
+++ ext/trusted/src/ruby/config.rs
@@ -1,1 +1,13 @@
+use ruru::{Object, VerifiedObject};
+
 class!(Config);
+
+impl VerifiedObject for Config {
+    fn is_correct_type<T: Object>(object: &T) -> bool {
+        object.respond_to("binding_type") && object.respond_to("listen_on")
+    }
+
+    fn error_message() -> &'static str {
+        "Error converting to Config"
+    }
+}
