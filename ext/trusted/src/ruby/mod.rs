7ae41ef5015878c2fca7347fa6e1b99a1e170d99
Dmitry Gritsay <unseductable@gmail.com>
2016-10-25 22:43:26 +0300

    Create ruby::Config

--- ext/trusted/src/ruby/mod.rs
+++ ext/trusted/src/ruby/mod.rs
@@ -1,7 +1,9 @@
+mod config;
 mod request;
 mod response;
 mod server;
 
+pub use self::config::Config;
 pub use self::request::Request;
 pub use self::response::Response;
 pub use self::server::Server;
